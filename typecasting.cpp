ca5b872fc99dc276b91a57a94ee6f29d88a97204
dishvanthini <150140881+dishvanthini@users.noreply.github.com>
2024-08-13 12:00:31 +0530

    Create typecasting.cpp

---
+++ typecasting.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+int main (){
+	int a,b;
+	float diff;
+
+	cout<<"enter a :";
+	cin>>a;
+	cout<<"enter b :";
+	cin>>b;
+	cout<<"division :";
+	cin>>div;
+	div=float(a/b);
+	cout<<"\ndivision :";
+		cout<<div;
+
+	return 0;
+	
+}
+
