aa47b03651bb3bc391cc63b9c43e249bc2fd8950
dishvanthini <150140881+dishvanthini@users.noreply.github.com>
2024-08-14 12:04:29 +0530

    Create distancebtw_2point.cpp

---
+++ distancebtw_2point.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cmath>// c prefix insted of .h 
+using namespace std;
+
+class point{
+	public:
+		int x,y;
+		
+		point(int l,int m )
+		{
+			x=l;
+			y=m;
+			
+		}
+		void display(){
+			cout<<"l: "<<x<<endl<<"m :"<<y<<endl;
+			
+			
+		}
+		float distance(point p,point q){
+			
+			float d=sqrt(pow((p.x-q.x),2)+pow((p.x+p.y),2)); //math header file 
+			cout<<endl<<"distance : "<<d<<'\n';
+			return d;
+		}
+};
+
+int main(){
+	point p(7,3);
+	p.display();
+	
+	
+	point q(8,5);
+	q.display();
+	
+	p.distance( p, q);
+	
+	q.distance(p,q);
+	//int d=sqrt(pow((p.x-q.x),2)+pow((p.x+p.y),2));
+	
+	
+	
+	
+	
+	return 0 ;
+}
+//constructor
