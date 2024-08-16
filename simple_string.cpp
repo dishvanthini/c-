a2ed0ee60d6bce25d038e4da72f75e102a05024b
dishvanthini <150140881+dishvanthini@users.noreply.github.com>
2024-08-16 11:14:03 +0530

    Create simple_string.cpp

---
+++ simple_string.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+
+int main(){
+	
+	string c="2";
+	string  d="3";
+	
+	int e=2;
+	int f=3;
+	
+	cout<<e+f<<endl;
+	cout<<c+d;
+	
+	//cout<<string(c+d);
+	return 0;
+}
