375505e2bcb49fd09193dfbe28a9beae2b9d278e
DISHVANTHINI <150140881+dishvanthini@users.noreply.github.com>
2025-12-05 14:15:39 +0530

    Create 3d_with_pointer.c

---
+++ 3d_with_pointer.c
@@ -0,0 +1,18 @@
+int main() {
+    int arr[2][3][2] = {
+        { {1, 2}, {3, 4}, {5, 6} },
+        { {7, 8}, {9, 10}, {11, 12} }
+    };
+
+    // Pointer to a 2D array of size [3][2]
+    int (*ptr)[3][2] = arr;
+
+    // Print arr[1][1][0] and arr[1][1][1] using pointer arithmetic
+    printf("Values at arr[1][1][i]: ");
+
+    for (int i = 0; i < 2; i++) {
+        printf("%d ", *(*(*(ptr +1) + 2) + i));//
+    }
+
+    return 0;
+}
