86160b4dbf0fc1fa8380473198d86574a921ad94
DISHVANTHINI <150140881+dishvanthini@users.noreply.github.com>
2025-12-06 16:11:52 +0530

    Create return_in_forloop.c

---
+++ return_in_forloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%d ", i);
+
+        if (i == 3)
+            return 0;   // ends main and program
+    }
+
+    printf("I will never run!");
+}
